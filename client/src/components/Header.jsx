29d77d9210ba7972ccd6c9ba5bc1de1a10b27255
Christine Lin <christinelin@Christines-MacBook-Air.local>
2024-10-09 15:06:36 -0400

    Add new list and delete functionality

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function Header({updateLists}) {
+
+  // Get current date
+  const today = new Date().toLocaleDateString("default", {
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
+
+  const [list, setList] = useState("");
+  
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    try {
+        const response = await fetch("/api/lists",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json", // Set the content type to JSON
+              },
+            body: JSON.stringify({
+                list:list
+            }),
+        });
+
+        const data = await response.json();
+        console.log("New list added: ", data);
+        setList("");
+        updateLists();
+
+    } catch (err) {
+        console.error("Error adding list:", err);
+    }
+}
+
+  return (
+    <div>
+      <h1>Task Trackr</h1>
+      <p>{today}</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value = {list}
+          placeholder="Add a new list"
+          autoComplete="off"
+          onChange={(e) => setList(e.target.value)}
+        />
+        <button type="submit"> + New List</button>
+      </form>
+    </div>
+  );
+}
+
+export default Header;
--- server/server.js
+++ server/server.js
@@ -28,10 +28,18 @@ db.connect()
   .then(() => console.log('Connected to PostgreSQL'))
   .catch(err => console.error('Connection error', err.stack));
 
+let tasks = [
+    { id: 1, task: "Buy milk", list_id: "General" },
+    { id: 2, task: "Finish homework", list_id: "Learning" },
+];
+  
+let lists = [{id: 1, name: "Learning"},{id:2, name: "General"}]
+
+
 // Route to fetch all items from the "items" table
-app.get("/api/items", async (req, res) => {
+app.get("/api/tasks", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM items");
+    const result = await db.query("SELECT * FROM tasks");
 
     // Log the data received from the PostgreSQL database
     console.log("Data from Postgres:", result.rows);
@@ -41,6 +49,59 @@ app.get("/api/items", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get("/api/lists", async(req,res) => {
+  try {
+    const result = await db.query("SELECT * FROM lists");
+    console.log("Data from Postgres:", result.rows);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({error: 'Internal Server Error'})
+  }
+})
+
+app.post("/api/lists", async(req,res) => {
+  const {list} = req.body;
+
+  if(!list){
+    return res.status(400).json({error: "List name is required"});
+  }
+
+  try {
+    const result = await db.query("INSERT INTO lists (name) VALUES ($1) RETURNING *;", [list])
+    res.status(201).json(result.rows[0]);
+    
+  } catch (err) {
+    console.error("Error inserting list:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+})
+
+app.delete("/api/lists/:id", async (req, res) => {
+  try {
+    const { id } = req.params; // Extract the id from the request parameters
+
+    // Check for items in the list 
+    const hasTasks = await db.query("SELECT * FROM tasks WHERE list_id = ($1);", [id]);
+
+    if(hasTasks.rowCount > 0){
+      await db.query("DELETE FROM tasks WHERE list_id = ($1) RETURNING *;", [id]);
+    } else {
+      const result = await db.query("DELETE FROM lists WHERE id = ($1) RETURNING *", [id])
+      // Check if the row was successfully deleted
+      if (result.rowCount > 0) {
+        res.json({ success: true, message: "List deleted", deletedList: result.rows[0] });
+      } else {
+        res.status(404).json({ success: false, message: "List not found" });
+      }
+    }
+
+  } catch (err) {
+    console.error("Error deleting list:", err);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 });
 
