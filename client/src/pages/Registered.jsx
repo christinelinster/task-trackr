3d96c4c17ece1d85f068a6250127af9017440d5d
Christine Lin <lin.christine19@gmail.com>
2025-05-01 21:06:37 -0400

    update style for registered page

--- client/src/pages/Registered.jsx
+++ client/src/pages/Registered.jsx
@@ -1,9 +1,43 @@
-export default function Registered(){
-    return (
+import { Container, Box, Button} from "@mui/material";
+
+export default function Registered() {
+  return (
+    <Container maxWidth="xs">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="100vh"
+      >
+        <p>Thank you for registering!</p>
         <div>
-            <h2>Registered</h2>
-            <p>Thank you for registering!</p>
-            <a href="/login">Click here to login</a>
+        <Button
+              variant="outlined"
+              sx={{
+                flex: 1,
+                borderColor: "#DED0B680",
+                color: "#DED0B680",
+                "&:hover": {
+                  backgroundColor: "rgba(222, 208, 182, 0.75)",
+                  borderColor: "rgba(222, 208, 182, 0.75)",
+                  color: "rgba(255, 255, 255, 0.87)",
+                },
+                outline: "none",
+                "&:focus": {
+                  outline: "none",
+                  boxShadow: "none",
+                },
+              }}
+              component="a"
+              href="/login"
+            >
+              Login
+            </Button>
+
         </div>
-    );
+        
+      </Box>
+    </Container>
+  );
 }
